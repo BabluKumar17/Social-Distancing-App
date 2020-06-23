fb3222511c44867a1b74a5a247bb6afe8733581d
Bablu Kumar <hacback17@gmail.com>
2020-06-23 14:36:57 +0530

    Works fine

---
+++ app/src/main/java/com/security10x/socialdistancing/BluetoothService.java
@@ -0,0 +1,16 @@
+package com.security10x.socialdistancing;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+public class BluetoothService extends Service {
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
